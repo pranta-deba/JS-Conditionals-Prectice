541eeaca886baca00680d4d5095aef708559b02a
Pranto Deb <rajprit0m@outlook.com>
2024-01-27 17:37:08 +0600

    1st task completed

--- Free_Drinks.js
+++ Free_Drinks.js
@@ -5,4 +5,13 @@ Free Drinks
     - Else Coke: 30tk
 */
 
-            
+ let cokeRate = 30;
+ let maxBuy = 500;
+ let payment = 54;
+
+ if (payment >= maxBuy) {
+    console.log(`You spent ${payment}. That's why you got a ${cokeRate} taka free coke.Then you have to give opinion in ${payment} taka`);
+ }else{
+    payment += 30;
+    console.log(`Sorry, don't spend more than ${maxBuy} taka. So you won't get free coke. ${cokeRate} more for coke. Then you have to give opinion in ${payment} taka`);
+ }
